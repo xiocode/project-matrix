f7d5180ecba0055ace4fa70d3ec8aba17cfd61dd
Tony.Shao <xiocode@gmail.com>
2024-10-15 18:35:04 +0800

    调整工单接口

--- app/_definitions/models/entity-watchers/mom/MomWorkTask.ts
+++ app/_definitions/models/entity-watchers/mom/MomWorkTask.ts
@@ -32,43 +32,32 @@ export default [
           }
         }
       }
-
-
-      const routeProcess = await server.getEntityManager("mom_route_process").findEntity({
-        filters: [
-          { operator: "eq", field: "id", value: before.routeProcess },
-        ],
-        properties: ["id", "process"]
-
-      });
-
-      if (routeProcess) {
-        before.process_id = routeProcess?.process?.id
-      }
     }
   },
   {
-    eventName: "entity.beforeUpdate",
+    eventName: "entity.update",
     modelSingularCode: "mom_work_task",
-    handler: async (ctx: EntityWatchHandlerContext<"entity.beforeUpdate">) => {
+    handler: async (ctx: EntityWatchHandlerContext<"entity.update">) => {
       const { server, payload } = ctx;
-      let before = payload.before;
+      let {before, changes} = payload;
 
-      if (!before.hasOwnProperty("workOrder") && !before.hasOwnProperty("work_order_id")) {
-        const workOrderManager = server.getEntityManager<MomWorkOrder>("mom_work_order");
-        const workOrder = await workOrderManager.findEntity({
-          filters: [
-            { operator: "eq", field: "id", value: before.workOrder.id || before.workOrder || before.work_order_id },
-            { operator: "eq", field: "executionState", value: 'processing' },
-          ],
-        });
-        if (workOrder) {
-          await workOrderManager.updateEntityById({
-            id: workOrder.id,
-            entityToSave: {
-              executionState: 'completed',
-            }
-          })
+      if (before.hasOwnProperty("workOrder") || before.hasOwnProperty("work_order_id")) {
+        if (changes.hasOwnProperty("executionState") && changes.executionState === "completed") {
+          const workOrderManager = server.getEntityManager<MomWorkOrder>("mom_work_order");
+          const workOrder = await workOrderManager.findEntity({
+            filters: [
+              { operator: "eq", field: "id", value: before.workOrder.id || before.workOrder || before.work_order_id },
+              { operator: "eq", field: "executionState", value: 'processing' },
+            ],
+          });
+          if (workOrder) {
+            await workOrderManager.updateEntityById({
+              id: workOrder.id,
+              entityToSave: {
+                executionState: 'completed',
+              }
+            })
+          }
         }
       }
     }
